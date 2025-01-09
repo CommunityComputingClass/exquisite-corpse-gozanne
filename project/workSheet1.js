c7ca07f4a7147ed30c3778efb744757f45ac7f1a
gozanne <25gavin.ozanne@lwhs.org>
2025-01-09 18:03:16 +0000

    Added Worksheet 2

--- project/workSheet1.js
+++ project/workSheet1.js
@@ -4,12 +4,11 @@ function setup() {
 }
 
 function draw() {
-  /*square(10, 10, 50);
+  square(10, 10, 50);
   square(440, 440, 50);
   strokeWeight(5);
   line(200, 200, 200, 150);
   line(200, 150, 300, 200);
   strokeWeight(1);
-  line(250, 200, 350, 100);*/
-  
+  line(250, 200, 350, 100);x
 }
--- project/workSheet2.js
+++ project/workSheet2.js
@@ -1,10 +1,27 @@
 function setup() {
-    createCanvas(500, 500);
-    background( 146, 232, 201 );
-  }
-  
-  function draw() {    
-  }
+  createCanvas(400, 400);
+  background( 146, 232, 201 );
+}
 
+function draw() {  
+  eye(200, 150)  
+}
 
+function eye(x, y) { // Creates Eye Centered on X, Y coords
+  stroke("blue")
+  fill("white")
+  ellipse((x-40), y, 50, 20)
+  fill("green")
+  circle((x-40), y, 20)
+  fill("black")
+  circle((x-42.5), (y-2.5), 5)
+
+  stroke("blue")
+  fill("white")
+  ellipse((x+40), y, 50, 20)
+  fill("green")
+  circle((x+40), y, 20)
+  fill("black")
+  circle((x+37.5), (y-2.5), 5)
+}
   
