60225268dbeff201605be954695f0c132b53f0ef
gozanne <25gavin.ozanne@lwhs.org>
2025-01-09 17:34:29 +0000

    submitting my first code

---
+++ project/workSheet1.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(500, 500);
+  background( 146, 232, 201 );
+}
+
+function draw() {
+  /*square(10, 10, 50);
+  square(440, 440, 50);
+  strokeWeight(5);
+  line(200, 200, 200, 150);
+  line(200, 150, 300, 200);
+  strokeWeight(1);
+  line(250, 200, 350, 100);*/
+  
+}
---
+++ project/workSheet2.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(500, 500);
+    background( 146, 232, 201 );
+  }
+  
+  function draw() {    
+  }
+
+
+  
